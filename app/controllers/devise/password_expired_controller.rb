f71d8ce185a8387860342739437268df2a12cec3
Henry Keiter <henry@apsis.io>
2017-03-14 13:10:26 -0600

    Remove this damn thing.

--- app/controllers/devise/password_expired_controller.rb
+++ app/controllers/devise/password_expired_controller.rb
@@ -8,7 +8,6 @@ class Devise::PasswordExpiredController < DeviseController
   end
 
   def update
-    resource.extend(Devise::Models::DatabaseAuthenticatablePatch)
     if resource.update_with_password(resource_params)
       warden.session(scope)['password_expired'] = false
       set_flash_message :notice, :updated
